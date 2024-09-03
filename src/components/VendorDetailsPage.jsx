5651d77707ded8c13d7ed5dd2545ef0630165cd4
RohitKalkumbe <115150885+RohitKalkumbe@users.noreply.github.com>
2024-09-03 11:28:29 +0530

    Update VendorDetailsPage.jsx

--- src/components/VendorDetailsPage.jsx
+++ src/components/VendorDetailsPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
-import VendorTable from './VendorManagement';
 const Container = styled.div`
     margin: 20px;
 `;
